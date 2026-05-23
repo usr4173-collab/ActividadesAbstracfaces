32bf0625ebf7da1e4261cae5c6e4acf984e6b073
usr4173-collab <usr4173@salesianos-lacuesta.net>
2026-05-23 17:48:43 +0100

    He creado la clase Circulo y he implementado los métodos de CalculoGeometrico

---
+++ src/net/salesianos/actividad1/Circulo.java
@@ -0,0 +1,21 @@
+package net.salesianos.actividad1;
+
+public class Circulo implements CalculoGeometrico {
+
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+    
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+    
+}
