d9a4968270b6979790dd799aa15bc7da84daba8f
usr4173-collab <usr4173@salesianos-lacuesta.net>
2026-05-23 17:56:49 +0100

    He terminado la actividad 1 implementando en el main los métodos de Cálculo, círculo y rectángulo y he comprobado que funcionan.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+import net.salesianos.actividad1.Rectangulo;
+import net.salesianos.actividad1.Circulo;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner usuario = new Scanner(System.in);
+
+        // Clase rectángulo:
+
+        System.out.println("Dime la base de un rectángulo: ");
+        double baseRec = usuario.nextDouble();
+
+        System.out.println("Dime la base de un rectángulo: ");
+        double baseAlt= usuario.nextDouble();
+
+        Rectangulo rec = new Rectangulo(baseRec, baseAlt);
+
+        System.out.println("Base: " + baseRec +
+            "\nAltura: " + baseAlt + "\nÁrea: " + rec.calcularArea()); 
+        System.out.println("Perímetro: " + rec.calcularPerimetro());
+
+        //Clase círculo:
+
+        System.out.println("\nDime el área de un círculo:");
+        double radio = usuario.nextDouble();
+
+        Circulo circ = new Circulo(radio);
+
+        System.out.println("Radio: " + radio +
+            "\nÁrea: " + circ.calcularArea() + "\nÁrea: " + circ.calcularArea()); 
+        System.out.println("Perímetro: " + circ.calcularPerimetro());
+
+        usuario.close();
     }
 }
