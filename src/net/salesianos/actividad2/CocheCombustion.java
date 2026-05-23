d63d93b2c8ce2ba99d5b9603091f365d43c6d027
usr4173-collab <usr4173@salesianos-lacuesta.net>
2026-05-23 20:34:27 +0100

    He creado la clase CocheCombustion que se extiende de Vehiculo, he añadido sus atributos, getters setters y sobreescrito los métodos necesarios.

---
+++ src/net/salesianos/actividad2/CocheCombustion.java
@@ -0,0 +1,73 @@
+package net.salesianos.actividad2;
+
+public class CocheCombustion extends Vehiculo {
+
+    private boolean motorEncendido;
+    private int litrosCombustible;
+
+    public CocheCombustion(String color, int numeroPuertas, int numeroRuedas, 
+        String modelo, String matricula,int litrosCombustible) {
+
+        super(color, numeroPuertas, numeroRuedas, modelo, matricula);
+
+        this.litrosCombustible = litrosCombustible;
+        this.motorEncendido = false;
+    }
+
+        public int getLitrosCombustible() {
+        return litrosCombustible;
+    }
+
+    public void setLitrosCombustible(int litrosCombustible) {
+        this.litrosCombustible = litrosCombustible;
+    }
+    
+
+    @Override
+    public void arrancar() {
+
+        if (litrosCombustible > 0) {
+
+            litrosCombustible--;
+            motorEncendido = true;
+
+            System.out.println("Motor arrancado.");
+
+        } else {
+
+            System.out.println("No hay gasoflina.");
+        }
+    }
+
+
+    @Override
+    public void pararMotor() {
+
+        motorEncendido = false;
+
+        System.out.println("Motor apagado.");
+    }
+
+    @Override
+    public void mover() {
+
+        if (!motorEncendido) {
+
+            System.out.println("El coche no puede moverse. Se ha apagado el motor.");
+            return;
+        }
+
+        if (litrosCombustible <= 0) {
+
+            System.out.println("Sin gasofa.");
+            pararMotor();
+            return;
+        }
+
+        litrosCombustible--;
+
+        System.out.println("El coche se ha movido.");
+    }
+
+    
+}
